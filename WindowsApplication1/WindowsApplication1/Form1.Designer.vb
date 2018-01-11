fc855e06ba866559bfae47bfb43dcfcb4881bcb7
MERCURY\AA004268754 <tessa.academia@gmail.com>
2018-01-12 00:31:18 +0800

    AMTLA 01.12.2018

--- WindowsApplication1/WindowsApplication1/Form1.Designer.vb
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -22,9 +22,29 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnHello = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnHello
+        '
+        Me.btnHello.Location = New System.Drawing.Point(108, 107)
+        Me.btnHello.Name = "btnHello"
+        Me.btnHello.Size = New System.Drawing.Size(75, 23)
+        Me.btnHello.TabIndex = 0
+        Me.btnHello.Text = "Hello"
+        Me.btnHello.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.btnHello)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents btnHello As System.Windows.Forms.Button
 
 End Class
