388868540c15e6f1335f9f251b9e7586b5c4459b
Robert John Millano <bemanj87@gmail.com>
2018-01-12 00:27:23 +0800

    add lastname property

--- WindowsApplication1/WindowsApplication1/Class1.vb
+++ WindowsApplication1/WindowsApplication1/Class1.vb
@@ -1,3 +1,12 @@
 ﻿Public Class Class1
+    Private _lastname As String
+    Public Property LastName() As String
+        Get
+            Return _lastname
+        End Get
+        Set(ByVal value As String)
+            _lastname = value
+        End Set
+    End Property
 
 End Class
